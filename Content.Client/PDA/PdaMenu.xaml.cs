f0e049048b338f055bca400f9ea519487d25e416
Enoken-crypto <68296202+Enoken-crypto@users.noreply.github.com>
2025-06-01 21:34:10 +0300

    the beginning

--- Content.Client/PDA/PdaMenu.xaml.cs
+++ Content.Client/PDA/PdaMenu.xaml.cs
@@ -32,7 +32,7 @@ namespace Content.Client.PDA
         private string _stationName = Loc.GetString("comp-pda-ui-unknown");
         private string _alertLevel = Loc.GetString("comp-pda-ui-unknown");
         private string _instructions = Loc.GetString("comp-pda-ui-unknown");
-        
+
 
         private int _currentView;
 
@@ -126,7 +126,7 @@ namespace Content.Client.PDA
                 _clipboard.SetText(_instructions);
             };
 
-            
+
 
 
             HideAllViews();
@@ -161,7 +161,7 @@ namespace Content.Client.PDA
             _stationName = state.StationName ?? Loc.GetString("comp-pda-ui-unknown");
             StationNameLabel.SetMarkup(Loc.GetString("comp-pda-ui-station",
                 ("station", _stationName)));
-            
+
 
             var stationTime = _gameTiming.CurTime.Subtract(_gameTicker.RoundStartTimeSpan);
 
@@ -192,6 +192,7 @@ namespace Content.Client.PDA
             EjectPassportButton.IsActive = state.HasPassport; // WD EDIT
             ActivateMusicButton.Visible = state.CanPlayMusic;
             ShowUplinkButton.Visible = state.HasUplink;
+            ShowContractsButton.Visible = state.HasUplink; // WD edit - contractors
             LockUplinkButton.Visible = state.HasUplink;
         }
 
